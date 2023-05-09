3e29d32b74d5db87b4bce8473a83a0defbf0e825
krivig <krivig123@gmail.com>
2023-05-09 06:58:37 +0000

    started project

---
+++ index.php
@@ -0,0 +1,12 @@
+<?php
+session_start();
+
+// Include functions
+include 'functions.php';
+$mysqli = mysqli_connect_mysql();
+// connect to the database using PDO MySQL
+$pdo = pdo_connect_mysql();
+// Page is set to home (home.php) by default, so when the visitor visits that will be the page they see.
+$page = isset($_GET['page']) && file_exists('pages/' . $_GET['page'] . '.php') ? $_GET['page'] : 'home';
+// Include and show the requested page
+include 'pages/' . $page . '.php';
