8d2140560413f6e57bf2aad18c684740898057b2
krivig <krivig123@gmail.com>
2023-05-10 10:04:29 +0000

    login works

--- pages/login/login.php
+++ pages/login/login.php
@@ -5,41 +5,70 @@ if (isset($_SESSION['loggedin'])) {
     exit;
 }
 
-headerr('Logg inn');
+headerr('sign in');
 ?>
 
 <?php
-// If the user is not logged in redirect to the login page...
-if (isset($_SESSION['loggedin'])) {
-    header('Location: index.php');
-    exit;
-}
 
-headerr('Logg inn');
-?>
+if(isset($_POST['submit'])){  
+    //ensure fields are not empty
+    $username = !empty($_POST['username']) ? trim($_POST['username']) : null;
+    $passwordAttempt = !empty($_POST['password']) ? trim($_POST['password']) : null;
 
-<?php
-$stage = ($_GET['stage']);
-$stageArr = ["", "Venligst fyll ut begge feltene", "Feil brukernavn og eller passord", "Registering fullført, du kan nå logge inn"];
+    $sql = "SELECT id, username, password FROM users WHERE username = :username";
+    $stmt = $pdo->prepare($sql);
 
-if (isset($stage)) {
-    $msg = $stageArr["$stage"];
-} 
-else {
-    $msg = "";
+    //Bind value.
+    $stmt->bindValue(':username', $username);
+
+    //Execute.
+    $stmt->execute();
+    
+    //Fetch row.
+    $user = $stmt->fetch(PDO::FETCH_ASSOC);
+    print_r($user);
+
+    $id = $user["id"];
+    $username = $user["username"];
+
+    echo "<br><br>";
+    echo $id;
+    echo "<br>";
+    echo $username;
+
+
+    //If $row is FALSE.
+    if($user === false){
+        echo '<script>alert("invalid username or password")</script>';
+    } else{
+        //Compare and decrypt passwords.
+        $validPassword = password_verify($passwordAttempt, $user['password']);
+
+        //If $validPassword is TRUE, the login has been successful.
+        if($validPassword){
+            
+            //Provide the user with a login session.
+            $_SESSION['loggedin'] = true;
+            $_SESSION["id"] = $id;
+            $_SESSION["username"] = $username;
+            header('Location: index.php');
+            exit;
+
+        } else{
+            //$validPassword was FALSE. Passwords do not match.
+            echo '<script>alert("invalid username or password")</script>';
+        }
+    }
 }
 
 ?>
 <div class="content-wrapper-center">
     <div class="box">
         <h1>Log in</h1>
-        <form action="index.php?page=login/authlogin" method="post">
-            <input type="text" name="username" placeholder="Username" id="username" required>
-            <input type="password" name="password" placeholder="Password" id="password" required>
-            <p><?= $msg ?></p>
-            <input type="submit" value="Log in">
+        <form action="" method="post">                          
+            <input type="text" name="username" placeholder="Username">
+            <input type="password" name="password" placeholder="Password">
+            <button name="submit" type="submit">sign in</button>
         </form>
     </div>
 </div>
-
-<?= template_footer() ?>
---
+++ pages/login/register.php
@@ -0,0 +1,65 @@
+<?php
+// If the user is not logged in redirect to the login page...
+if (isset($_SESSION['loggedin'])) {
+    header('Location: index.php');
+    exit;
+}
+
+headerr('Register');
+?>
+
+<?php
+if(isset($_POST['submit'])) {  
+    try {
+        $user = $_POST['username'];
+        $email = $_POST['email'];
+        $pass = $_POST['password'];
+
+        //encrypt password
+        $pass = password_hash($pass, PASSWORD_BCRYPT, array("cost" => 12));
+
+        //Check if username exists
+        $sql = "SELECT COUNT(username) AS num FROM users WHERE username = :username";
+        $stmt = $pdo->prepare($sql);
+
+        $stmt->bindValue(':username', $user);
+        $stmt->execute();
+        $row = $stmt->fetch(PDO::FETCH_ASSOC);
+
+        if($row['num'] > 0) {
+            echo '<script>alert("Username already exists")</script>';
+        }
+        else {
+            $stmt = $pdo->prepare("INSERT INTO users (username, email, password)
+            VALUES (:username, :email, :password)");
+            $stmt->bindParam(':username', $user);
+            $stmt->bindParam(':email', $email);
+            $stmt->bindParam(':password', $pass);
+
+            if($stmt->execute()){
+                echo '<script>alert("New account created.")</script>';
+                //redirect to another page
+                header("index.php");
+            }
+            else {
+               echo '<script>alert("An error occurred")</script>';
+            }
+        }
+    }catch(PDOException $e){
+        $error = "Error: " . $e->getMessage();
+        echo '<script type="text/javascript">alert("'.$error.'");</script>';
+    }
+}
+
+?>
+<div class="content-wrapper-center">
+    <div class="box">
+        <h1>Register</h1>
+        <form action="" method="post">
+            <input type="text" required="required" name="username" placeholder="Username">
+            <input required="required" type="email" name="email" placeholder="Email">
+            <input required="required" type="password" name="password" placeholder="Password">                  
+            <button name="submit" type="submit">register</button>
+        </form>
+      </div>
+</div>
