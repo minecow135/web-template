ac6629802275a40674935e086553ec9764453ecc
mine_cow135 <minecow135>
2023-05-22 06:47:29 +0200

    started registercodes page

--- pages/adm/registerCodes/list.php
+++ pages/adm/registerCodes/list.php
@@ -3,7 +3,7 @@ headerr('Register codes', "registerCodes");
 ?>
 
 <?php
-$sql = "SELECT registerCodes.id, registerCodes.code, registerCodes.createdBy, users.username, registerCodes.start, registerCodes.end FROM registerCodes LEFT JOIN users ON registerCodes.createdBy = users.id";
+$sql = "SELECT registerCodes.id, registerCodes.code, registerCodes.createdBy, users.username, registerCodes.usesLeft, registerCodes.start, registerCodes.end FROM registerCodes LEFT JOIN users ON registerCodes.createdBy = users.id";
         $stmt = $pdo->prepare($sql);
 
         //Execute.
@@ -11,28 +11,96 @@ $sql = "SELECT registerCodes.id, registerCodes.code, registerCodes.createdBy, us
 
         //Fetch row.
         $code = $stmt->fetchAll(PDO::FETCH_ASSOC);
+
+        $allcodes = in_array("registerCodes.all", array_column($_SESSION["permissions"], 'permissionName'));
+$date = date("Y-m-d H:i:s");
 ?>
 
 <div class="content-wrapper-center">
     <h1 class="head">Register codes list</h1>
-    <table border="1">
-        <tr>
-            <th>Id</th>
-            <th>Code</th>
-            <th>Valid from</th>
-            <th>Valid to</th>
-            <th>created by</th>
-        </tr>
+    <div class="createCode">
+        <form action="" method="post">
+            <label for="uses">Uses: </label>
+            <input type="number" name="uses" id="uses">
+            <button type="submit">Create</button>
+        </form>
+    </div>
+    <h3>Active codes</h3>
+    <table>
+        <thead>
+            <tr>
+                <td>Id</td>
+                <td>Code</td>
+                <td>Uses left</td>
+                <td>Valid from</td>
+                <td>Valid to</td>
+                <?php
+                if ($allcodes) {
+                    ?>
+                <td>created by</td>
+            <?php
+                }
+            ?>
+            </tr>
+        </thead>
     <?php
         foreach ($code as $i) {
-            if (($i["createdBy"] == $_SESSION["id"]) || (in_array("registerCodes.all", array_column($_SESSION["permissions"], 'permissionName')))) {
+            if ((($i["createdBy"] == $_SESSION["id"]) || $allcodes) && 0 < $i["usesLeft"] && $date >= $i["start"] && $date <= $i["end"]) {
                 ?>
                 <tr>
                     <td><?= $i["id"] ?></td>
                     <td><?= $i["code"] ?></td>
+                    <td><?= $i["usesLeft"] ?></td>
                     <td><?= $i["start"] ?></td>
                     <td><?= $i["end"] ?></td>
+                    <?php
+                if ($allcodes) {
+                    ?>
                     <td><?= $i["username"] ?></td>
+                    <?php
+                }
+                    ?>
+                </tr>
+                <?php
+            }
+        }
+    ?>
+ <!--   </table>
+    <h3>Outdated codes</h3>
+    <table>
+        <thead>
+            <tr>
+                <td>Id</td>
+                <td>Code</td>
+                <td>Uses left</td>
+                <td>Valid from</td>
+                <td>Valid to</td>
+                <?php
+                if ($allcodes) {
+                    ?>
+                <td>created by</td>
+            <?php
+                }
+            ?>
+            </tr>
+        </thead>-->
+    <?php
+        foreach ($code as $i) {
+            if ((($i["createdBy"] == $_SESSION["id"]) || $allcodes) && 0 < $i["usesLeft"] && $date >= $i["start"] && $date >= $i["end"]) {
+                ?>
+                <tr>
+                    <td><?= $i["id"] ?></td>
+                    <td><?= $i["code"] ?></td>
+                    <td><?= $i["usesLeft"] ?></td>
+                    <td><?= $i["start"] ?></td>
+                    <td><?= $i["end"] ?></td>
+                    <?php
+                if ($allcodes) {
+                    ?>
+                    <td><?= $i["username"] ?></td>
+                    <?php
+                }
+                    ?>
                 </tr>
                 <?php
             }
