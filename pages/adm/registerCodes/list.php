67db9c39280a5bfb4032d54a6f72351cbcaaf52e
mine_cow135 <minecow135>
2023-05-30 14:55:07 +0200

    working list and create. started pages

--- pages/adm/registerCodes/list.php
+++ pages/adm/registerCodes/list.php
@@ -3,52 +3,97 @@ headerr('Register codes', "registerCodes");
 ?>
 
 <?php
-$sql = "SELECT registerCodes.id, registerCodes.code, registerCodes.createdBy, users.username, registerCodes.usesLeft, registerCodes.start, registerCodes.end FROM registerCodes LEFT JOIN users ON registerCodes.createdBy = users.id";
-        $stmt = $pdo->prepare($sql);
-
-        //Execute.
-        $stmt->execute();
-
-        //Fetch row.
-        $code = $stmt->fetchAll(PDO::FETCH_ASSOC);
-
-        $allcodes = in_array("registerCodes.all", array_column($_SESSION["permissions"], 'permissionName'));
 $date = date("Y-m-d H:i:s");
+
+$d=strtotime("+15 min");
+$date15 = date("Y-m-d H:i:s", $d);
+
+if (isset($_POST["disable"])) {
+    $stmt = $pdo->prepare("UPDATE registerCodes SET usesLeft = 0 WHERE id = :id");
+        $stmt->bindParam(':id', $_POST["disable"]);
+
+        if($stmt->execute()){
+            echo '<script>alert("Code disabled.")</script>';
+        }
+}
+
+$code = uniqid();
+
+if ($_POST["uses"]) {
+    $stmt = $pdo->prepare("INSERT INTO registerCodes (code, createdBy, usesLeft, start, end)
+        VALUES (:code, :createdBy, :usesLeft, :start, :end)");
+        $stmt->bindParam(':code', $code);
+        $stmt->bindParam(':createdBy', $_SESSION["id"]);
+        $stmt->bindParam(':usesLeft', $_POST["uses"]);
+        $stmt->bindParam(':start', $_POST["start"]);
+        $stmt->bindParam(':end', $_POST["end"]);
+
+        if($stmt->execute()){
+            echo '<script>alert("New account created.")</script>';
+        }
+}
+
+$sql = "SELECT registerCodes.id, registerCodes.code, registerCodes.createdBy, users.username, registerCodes.usesLeft, registerCodes.start, registerCodes.end FROM registerCodes LEFT JOIN users ON registerCodes.createdBy = users.id ORDER BY `registerCodes`.`start` DESC";
+    $stmt = $pdo->prepare($sql);
+
+    //Execute.
+    $stmt->execute();
+
+    //Fetch row.
+    $code = $stmt->fetchAll(PDO::FETCH_ASSOC);
+
+$allcodes = in_array("registerCodes.all", array_column($_SESSION["permissions"], 'permissionName'));
 ?>
 
 <div class="content-wrapper-center">
     <h1 class="head">Register codes list</h1>
+</div>
+<div class="content-wrapper2">
     <div class="createCode">
         <form action="" method="post">
-            <label for="uses">Uses: </label>
-            <input type="number" name="uses" id="uses">
-            <button type="submit">Create</button>
+            <table>
+                <thead>
+                    <tr>
+                        <td><label for="uses">Uses: </label></td>
+                        <td><label for="start">Start: </label></td>
+                        <td><label for="end">End: </label></td>
+                        <td></td>
+                    </tr>
+                </thead>
+                <tr>
+                    <td><input type="number" name="uses" id="uses" value="1" min="1"></td>
+                    <td><input type="datetime-local" name="start" id="start" value="<?= $date ?>" min="<?= $date ?>"></td>
+                    <td><input type="datetime-local" name="end" id="end" value="<?= $date15 ?>" min="<?= $date ?>"></td>
+                    <td><button type="submit">Create</button></td>
+                </tr>
+            </table>
         </form>
     </div>
     <h3>Active codes</h3>
-    <table>
-        <thead>
-            <tr>
-                <td>Id</td>
-                <td>Code</td>
-                <td>Uses left</td>
-                <td>Valid from</td>
-                <td>Valid to</td>
-                <?php
+    <form action="" method="post">
+        <table>
+            <thead>
+                <tr>
+                    <td>Code</td>
+                    <td>Uses left</td>
+                    <td>Valid from</td>
+                    <td>Valid to</td>
+                    <?php
                 if ($allcodes) {
                     ?>
-                <td>created by</td>
-            <?php
+                    <td>created by</td>
+                <?php
                 }
-            ?>
-            </tr>
-        </thead>
-    <?php
+                ?>
+                    <td></td>
+                    <td></td>
+                </tr>
+            </thead>
+        <?php
         foreach ($code as $i) {
             if ((($i["createdBy"] == $_SESSION["id"]) || $allcodes) && 0 < $i["usesLeft"] && $date >= $i["start"] && $date <= $i["end"]) {
                 ?>
                 <tr>
-                    <td><?= $i["id"] ?></td>
                     <td><?= $i["code"] ?></td>
                     <td><?= $i["usesLeft"] ?></td>
                     <td><?= $i["start"] ?></td>
@@ -60,17 +105,19 @@ $date = date("Y-m-d H:i:s");
                     <?php
                 }
                     ?>
+                    <td><button name="disable" value="<?= $i["id"] ?>" type="submit">Disable</button></td>
+                    <td><a href="index.php?page=adm/registerCodes/used&id=<?= $i["id"] ?>">view info</a></td>
                 </tr>
                 <?php
             }
         }
-    ?>
- <!--   </table>
-    <h3>Outdated codes</h3>
+        ?>
+        </table>
+    </form>
+    <h3>Outdated or used codes</h3>
     <table>
         <thead>
             <tr>
-                <td>Id</td>
                 <td>Code</td>
                 <td>Uses left</td>
                 <td>Valid from</td>
@@ -82,14 +129,14 @@ $date = date("Y-m-d H:i:s");
             <?php
                 }
             ?>
+                <td></td>
             </tr>
-        </thead>-->
+        </thead>
     <?php
         foreach ($code as $i) {
-            if ((($i["createdBy"] == $_SESSION["id"]) || $allcodes) && 0 < $i["usesLeft"] && $date >= $i["start"] && $date >= $i["end"]) {
+            if ((($i["createdBy"] == $_SESSION["id"]) || $allcodes) && (0 < $i["usesLeft"] && $date >= $i["start"] && $date >= $i["end"] ) || 1 > $i["usesLeft"]) {
                 ?>
                 <tr>
-                    <td><?= $i["id"] ?></td>
                     <td><?= $i["code"] ?></td>
                     <td><?= $i["usesLeft"] ?></td>
                     <td><?= $i["start"] ?></td>
@@ -98,6 +145,7 @@ $date = date("Y-m-d H:i:s");
                 if ($allcodes) {
                     ?>
                     <td><?= $i["username"] ?></td>
+                    <td><a href="index.php?page=adm/registerCodes/used&id=<?= $i["id"] ?>">view info</a></td>
                     <?php
                 }
                     ?>
@@ -108,5 +156,17 @@ $date = date("Y-m-d H:i:s");
     ?>
     </table>
 </div>
+
+<?php
+    $page = 2;
+    $num_on_page = 5;
+
+    $array_start = ($page - 1) * $num_on_page;
+
+    $array_part = array_slice($code, $array_start, $num_on_page);
+    foreach($array_part as $b) {
+        echo "<br>" . $b["id"];
+    }
+?>
 
 <?= template_footer() ?>
---
+++ pages/adm/registerCodes/used.php
@@ -0,0 +1,88 @@
+<?php
+headerr('Codes used by', "registerCodes");
+?>
+
+<?php
+   $sql = "SELECT registerCodes.id, registerCodes.code, registerCodes.createdBy, users.username, registerCodes.usesLeft, registerCodes.start, registerCodes.end FROM registerCodes LEFT JOIN users ON registerCodes.createdBy = users.id WHERE registerCodes.id = :id ORDER BY `registerCodes`.`start` DESC";
+   $stmt = $pdo->prepare($sql);
+
+   $stmt->bindParam(':id', $_GET["id"]);
+
+   //Execute.
+   $stmt->execute();
+
+   //Fetch row.
+   $code = $stmt->fetch(PDO::FETCH_ASSOC); 
+
+   $sql = "SELECT registerCodesUsed.id, registerCodes.code, users.username, registerCodesUsed.time FROM registerCodesUsed LEFT JOIN users ON registerCodesUsed.userId = users.id LEFT JOIN registerCodes ON registerCodesUsed.codeId = registerCodes.id WHERE registerCodesUsed.codeId = :id";
+   $stmt = $pdo->prepare($sql);
+
+   $stmt->bindParam(':id', $_GET["id"]);
+
+   //Execute.
+   $stmt->execute();
+
+   //Fetch row.
+   $uses = $stmt->fetchAll(PDO::FETCH_ASSOC); 
+?>
+
+<div class="content-wrapper-center">
+    <h1 class="head">Template</h1>
+    <table>
+        <thead>
+            <tr>
+                <td>Name</td>
+                <td>Value</td>
+            </tr>
+        </thead>
+        <tbody>
+            <tr>
+                <td>Id</td>
+                <td><?= $code["id"] ?></td>
+            </tr>
+            <tr>
+                <td>Code</td>
+                <td><?= $code["code"] ?></td>
+            </tr>
+            <tr>
+                <td>Created by</td>
+                <td><?= $code["username"] ?></td>
+            </tr>
+            <tr>
+                <td>Uses left</td>
+                <td><?= $code["usesLeft"] ?></td>
+            </tr>
+            <tr>
+                <td>Start</td>
+                <td><?= $code["start"] ?></td>
+            </tr>
+            <tr>
+                <td>End</td>
+                <td><?= $code["end"] ?></td>
+            </tr>
+        </tbody>
+    </table>
+
+    <table>
+        <thead>
+            <tr>
+                <td>Used by</td>
+                <td>Time</td>
+            </tr>
+        </thead>
+        <tbody>
+            <?php
+                foreach ($uses as $i) {
+            ?>
+                    <tr>
+                        <td><?= $i["username"] ?></td>
+                        <td><?= $i["time"] ?></td>
+                    </tr>
+            <?php
+                }
+            ?>
+        </tbody>
+    </table>
+</div>
+
+<?= template_footer() ?>
