a42794517df855d5d7cb81a9da82c6bf17654132
mine_cow135 <minecow135>
2023-09-22 12:06:30 +0200

    changed variable names for merge. merged user and group permission pages

--- pages/adm/permissions/index.php
+++ pages/adm/permissions/index.php
@@ -3,6 +3,21 @@ headerr('Permissions', "permissions");
 ?>
 
 <?php
+    $permissionName = "permissions.list.global";
+    $global = ((in_array($permissionName, array_column($_SESSION["permissions"], 'permissionName'))) && $permissionName != "default");
+    
+    $permissionName = "permissions.list.all";
+    $all = ((in_array($permissionName, array_column($_SESSION["permissions"], 'permissionName'))) && $permissionName != "default");
+
+    $permissionName = "permissions.delete.userPerm";
+    $deleteUserPerm = ((in_array($permissionName, array_column($_SESSION["permissions"], 'permissionName'))) && $permissionName != "default");
+
+    $permissionName = "permissions.delete.groupPerm";
+    $deletegroupPerm = ((in_array($permissionName, array_column($_SESSION["permissions"], 'permissionName'))) && $permissionName != "default");
+
+    $permissionName = "permissions.delete.groupUser";
+    $deleteGroupUser = ((in_array($permissionName, array_column($_SESSION["permissions"], 'permissionName'))) && $permissionName != "default");
+
     $sql = "SELECT id, username FROM `users`";
         $stmt = $pdo->prepare($sql);
 
@@ -15,11 +30,20 @@ headerr('Permissions', "permissions");
         //Fetch row.
         $user = $stmt->fetchAll(PDO::FETCH_ASSOC);
     
-    $sql = "SELECT id, siteName FROM `sites`";
+    $sql = "SELECT id, siteName FROM `sites` WHERE (id = :siteId";
+    if ($global) {
+        $sql .= " OR id = '0'";
+    }
+
+    if ($all) {
+        $sql .= " OR id LIKE '%'";
+    }
+
+    $sql .= ")";
         $stmt = $pdo->prepare($sql);
 
        // $stmt->bindValue(':code', $_POST["code"]);
-       // $stmt->bindValue(':siteId', $_SESSION["siteId"]);
+        $stmt->bindValue(':siteId', $_SESSION["siteId"]);
 
         //Execute.
         $stmt->execute();
@@ -27,11 +51,21 @@ headerr('Permissions', "permissions");
         //Fetch row.
         $site = $stmt->fetchAll(PDO::FETCH_ASSOC);
 
-    $sql = "SELECT group_groups.id, sites.siteName, group_groups.groupName FROM `group_groups` LEFT JOIN sites ON sites.id = group_groups.siteId";
+    $sql = "SELECT group_groups.id, group_groups.groupName, sites.siteName FROM `group_groups` LEFT JOIN sites ON group_groups.siteId = sites.id WHERE (group_groups.siteId = :siteId";
+    if ($global) {
+        $sql .= " OR group_groups.siteId = '0'";
+    }
+
+    if ($all) {
+        $sql .= " OR group_groups.siteId LIKE '%'";
+    }
+
+    $sql .= ")";
+
         $stmt = $pdo->prepare($sql);
 
        // $stmt->bindValue(':code', $_POST["code"]);
-       // $stmt->bindValue(':siteId', $_SESSION["siteId"]);
+        $stmt->bindValue(':siteId', $_SESSION["siteId"]);
 
         //Execute.
         $stmt->execute();
@@ -55,21 +89,6 @@ headerr('Permissions', "permissions");
 ?>
 
 <?php
-    $permissionName = "permissions.list.global";
-    $global = ((in_array($permissionName, array_column($_SESSION["permissions"], 'permissionName'))) && $permissionName != "default");
-    
-    $permissionName = "permissions.list.all";
-    $all = ((in_array($permissionName, array_column($_SESSION["permissions"], 'permissionName'))) && $permissionName != "default");
-
-    $permissionName = "permissions.delete.userPerm";
-    $deleteUserPerm = ((in_array($permissionName, array_column($_SESSION["permissions"], 'permissionName'))) && $permissionName != "default");
-
-    $permissionName = "permissions.delete.groupPerm";
-    $deletegroupPerm = ((in_array($permissionName, array_column($_SESSION["permissions"], 'permissionName'))) && $permissionName != "default");
-
-    $permissionName = "permissions.delete.groupUser";
-    $deleteGroupUser = ((in_array($permissionName, array_column($_SESSION["permissions"], 'permissionName'))) && $permissionName != "default");
-
     if ($_POST["user"]) {
         $sql = "SELECT group_userGroup.id, group_userGroup.userId, users.username, group_userGroup.groupId, group_userGroup.dateStart, group_userGroup.dateEnd, group_groups.groupName, group_groups.siteId, sites.siteName FROM `group_userGroup` LEFT JOIN users ON group_userGroup.userId = users.id LEFT JOIN group_groups ON group_userGroup.groupId = group_groups.id LEFT JOIN sites ON group_groups.siteId = sites.id WHERE users.id = :userId AND (group_groups.siteId = :siteId";
             
@@ -116,6 +135,53 @@ headerr('Permissions', "permissions");
             //Fetch row.
             $userPermissions = $stmt->fetchAll(PDO::FETCH_ASSOC);
     }
+
+    if ($_POST["group"]) {
+        $sql = "SELECT group_userGroup.id, group_userGroup.userId, users.username, group_userGroup.groupId, group_userGroup.dateStart, group_userGroup.dateEnd, group_groups.groupName, group_groups.siteId, sites.siteName FROM `group_userGroup` LEFT JOIN users ON group_userGroup.userId = users.id LEFT JOIN group_groups ON group_userGroup.groupId = group_groups.id LEFT JOIN sites ON group_groups.siteId = sites.id WHERE group_groups.id = :groupId AND (group_groups.siteId = :siteId";
+            
+            if ($global) {
+                $sql .= " OR group_groups.siteId = '0'";
+            }
+
+            if ($all) {
+                $sql .= " OR group_groups.siteId LIKE '%'";
+            }
+
+            $sql .= ")";
+
+            $stmt = $pdo->prepare($sql);
+
+            $stmt->bindValue(':groupId', $_POST["group"]);
+            $stmt->bindValue(':siteId', $_SESSION["siteId"]);
+
+            //Execute.
+                $stmt->execute();
+
+                //Fetch row.
+                $groupUsers = $stmt->fetchAll(PDO::FETCH_ASSOC);
+
+        $sql = "SELECT group_permissions.id, group_permissions.groupId, group_permissions.permissionId, group_permissions.header, group_groups.groupName, group_groups.siteId, permission.permissionName, permission.page, permission.dropdown, permission.placement, sites.siteName FROM group_permissions LEFT JOIN group_groups ON group_permissions.groupId = group_groups.id LEFT JOIN permission ON group_permissions.permissionId = permission.id LEFT JOIN sites ON group_groups.siteId = sites.id WHERE group_groups.id = :groupId AND (group_groups.siteId = :siteId";
+        if ($global) {
+            $sql .= " OR group_groups.siteId = '0'";
+        }
+
+        if ($all) {
+            $sql .= " OR group_groups.siteId LIKE '%'";
+        }
+
+        $sql .= ")";
+        
+            $stmt = $pdo->prepare($sql);
+
+            $stmt->bindValue(':groupId', $_POST["group"]);
+            $stmt->bindValue(':siteId', $_SESSION["siteId"]);
+
+            //Execute.
+            $stmt->execute();
+
+            //Fetch row.
+            $groupPermissions = $stmt->fetchAll(PDO::FETCH_ASSOC);
+    }
 ?>
 
 <div class="content-wrapper-center">
@@ -124,11 +190,20 @@ headerr('Permissions', "permissions");
 <div class="content-wrapper-center">
     <div class="box">
         <form action="" method="post">
+            <h4>Select user or group</h4>
             <select name="user" id="">
                 <option value="" selected disabled hidden>User</option>
                 <?php
                     foreach ($user as $u) {
                         echo "<option value=" . $u["id"] . ">" . $u["username"] . "</option>";
+                    }
+                ?>
+            </select>
+            <select name="group" id="">
+                <option value="" selected disabled hidden>Group</option>
+                <?php
+                    foreach ($group as $g) {
+                        echo "<option value=" . $g["id"] . ">" . $g["siteName"] . ", " . $g["groupName"] . "</option>";
                     }
                 ?>
             </select>
@@ -203,7 +278,7 @@ headerr('Permissions', "permissions");
                                 <input type="datetime-local" name="end" id="start">
                             </td>
                             <td>
-                                <button type="submit" name="groupSubmit">Submit</button>
+                                <button type="submit" name="usergroupSubmit">Submit</button>
                             </td>
                         </form>
                     </tr>
@@ -305,7 +380,160 @@ headerr('Permissions', "permissions");
             <?php
         }
 
-        if (isset($_POST["groupSubmit"])) {
+        if ($_POST["group"]) {
+            ?>
+            <table>
+                <thead>
+                    <tr>
+                        <td>Id</td>
+                        <td>Site</td>
+                        <td>Group</td>
+                        <td>Permission</td>
+                        <td>Header</td>
+                        <td></td>
+                    </tr>
+                </thead>
+                <tbody>
+                    <?php
+                        foreach ($groupPermissions as $a) {
+                    ?>
+                            <tr>
+                                <td><?= $a['id'] ?></td>
+                                <td><?= $a['siteName'] ?></td>
+                                <td><?= $a['groupName'] ?></td>
+                                <td><?= $a['permissionName'] ?></td>
+                                <?php
+                                    if ($a['header'] == 1) {
+                                ?>
+                                    <td>Yes</td>
+                                <?php
+                                    }
+                                    else {
+                                ?>
+                                        <td></td>
+                                <?php
+                                    }
+                                ?>
+                                <td class="actions">
+                                    <?php
+                                        if ($deleteGroupUser) {
+                                    ?>
+                                    <a href="index.php?page=adm/<?= basename(__DIR__) ?>/deleteGroupPerm&id=<?= $a['id'] ?>" class="trash"><i class="fas fa-trash fa-xs"></i></a>
+                                    <?php
+                                        }
+                                    ?>
+                                </td>
+                            </tr>
+                    <?php
+                        }
+                    ?>
+                    <tr>
+                        <form action="" method="post">
+                            <td>Add new</td>
+                            <td></td>
+                            <td>
+                                <?= $_POST["group"] ?>
+                                <input type="hidden" name="group" value="<?= $_POST["group"] ?>">
+                            </td>
+                            <td>
+                                <select name="permission" id="">
+                                    <option value="" selected disabled hidden>Permission</option>
+                                    <?php
+                                        foreach ($permission as $p) {
+                                            echo "<option value=" . $p["id"] . ">" . $p["permissionName"] . "</option>";
+                                        }
+                                    ?>
+                                </select>
+                            </td>
+                            <td>
+                                <div>
+                                    <label class="switch">
+                                    <input type="checkbox" name="header">
+                                        <span class="slider round"></span>
+                                    </label>
+                                </div>
+                            </td>
+                            <td>
+                                <button type="submit" name="groupPermission">Submit</button>
+                            </td>
+                        </form>
+                    </tr>
+                </tbody>
+            </table>
+
+            <table>
+                <thead>
+                    <tr>
+                        <td>Id</td>
+                        <td>group</td>
+                        <td>Site</td>
+                        <td>Username</td>
+                        <td>Start</td>
+                        <td>End</td>
+                        <td></td>
+                    </tr>
+                </thead>
+                <tbody>
+                    <?php
+                        foreach ($groupUsers as $a) {
+                    ?>
+                            <tr>
+                                <td><?= $a['id'] ?></td>
+                                <td><?= $a['groupName'] ?></td>
+                                <td><?= $a['siteName'] ?></td>
+                                <td><?= $a['username'] ?></td>
+                                <td><?= $a['dateStart'] ?></td>
+                                <td><?= $a['dateEnd'] ?></td>
+                                <td class="actions">
+                                    <?php
+                                        if ($deleteGroupUser) {
+                                    ?>
+                                    <a href="index.php?page=adm/<?= basename(__DIR__) ?>/deleteGroup&id=<?= $a['id'] ?>" class="trash"><i class="fas fa-trash fa-xs"></i></a>
+                                    <?php
+                                        }
+                                    ?>
+                                </td>
+                            </tr>
+                    <?php
+                        }
+                    ?>
+                    <tr>
+                        <form action="" method="post">
+                            <td>Add new</td>
+                            <td>
+                                <?= $_POST["group"] ?>
+                                <input type="hidden" name="group" value="<?= $_POST["group"] ?>">
+                            </td>
+                            <td>
+                            </td>
+                            <td>
+                                <select name="user" id="">
+                                    <option value="" selected disabled hidden>Username</option>
+                                    <?php
+                                        foreach ($user as $u) {
+                                            echo "<option value=" . $u["id"] . ">" . $u["username"] . "</option>";
+                                        }
+                                    ?>
+                                </select>
+                            </td>
+                            <td>
+                                <input type="datetime-local" name="start" id="start" value="<?= $date ?>">
+                            </td>
+                            <td>
+                                <input type="datetime-local" name="end" id="start">
+                            </td>
+                            <td>
+                                <button type="submit" name="usergroupSubmit">Submit</button>
+                            </td>
+                        </form>
+                    </tr>
+                </tbody>
+            </table>
+
+            <?php
+        }
+
+        if (isset($_POST["usergroupSubmit"])) {
             if (!isset($_POST["end"]) || $_POST["end"] == "") {
                 $end = null;
             }
@@ -325,6 +553,7 @@ headerr('Permissions', "permissions");
                     echo "<meta http-equiv='refresh' content='0'>";
                 }
         }
+        
         if (isset($_POST["userSubmit"])) {
             if (!isset($_POST["end"]) || $_POST["end"] == "") {
                 $end = null;
@@ -348,6 +577,33 @@ headerr('Permissions', "permissions");
                 $stmt->bindValue(':header', $header);
                 $stmt->bindValue(':dateStart', $_POST["start"]);
                 $stmt->bindValue(':dateEnd', $end);
+     
+                if($stmt->execute()){
+                    echo '<script>alert("Permission added")</script>';
+                    echo "<meta http-equiv='refresh' content='0'>";
+                }
+        }
+
+        if (isset($_POST["groupPermission"])) {
+            if (!isset($_POST["end"]) || $_POST["end"] == "") {
+                $end = null;
+            }
+            else {
+                $end = $_POST["end"];
+            }
+            
+            if (!isset($_POST["header"]) || $_POST["header"] == "") {
+                $header = 0;
+            }
+            else {
+                $header = 1;
+            }
+        
+            $stmt = $pdo->prepare("INSERT INTO `group_permissions`(`groupId`, `permissionId`, `header`) VALUES (:groupId, :permissionId, :header)");
+        
+                $stmt->bindValue(':groupId', $_POST["group"]);
+                $stmt->bindValue(':permissionId', $_POST["permission"]);
+                $stmt->bindValue(':header', $header);
      
                 if($stmt->execute()){
                     echo '<script>alert("Permission added")</script>';
